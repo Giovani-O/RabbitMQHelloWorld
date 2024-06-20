1b5520aab64068eba3ade4ee105d6e25406de514
Giovani de Oliveira <giovani98@proton.me>
2024-06-20 17:25:50 -0300

    first commit

---
+++ Receive/Receive.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// Cria conexão com o node local do RabbitMQ
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Declara a fila
+channel.QueueDeclare(
+    queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages");
+
+// Obtem mensagens da fila
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received: {message}");
+};
+
+// Consome as mensagens da fila
+channel.BasicConsume(
+    queue: "hello",
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
---
+++ Send/Send.cs
@@ -0,0 +1,37 @@
+﻿/*
+    RabbitMQ Hello World
+    See documentation: https://www.rabbitmq.com/tutorials/tutorial-one-dotnet
+ */
+
+using System.Text;
+using RabbitMQ.Client;
+
+// Cria conexão com o node local do RabbitMQ
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Cria uma fila
+channel.QueueDeclare(
+    queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+// Cria uma mensagem
+Console.WriteLine("Insert a message: ");
+var message = Console.ReadLine();
+
+var body = Encoding.UTF8.GetBytes(message);
+
+// Envia a mensagem
+channel.BasicPublish(
+    exchange: string.Empty,
+    routingKey: "hello",
+    basicProperties: null,
+    body: body);
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
